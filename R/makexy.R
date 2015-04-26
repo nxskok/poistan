98c2a5394797ec93ff4b458b380e4d6c40a6aa52
Ken Butler <butler@utsc.utoronto.ca>
2015-04-26 12:35:53 -0400

    Remade data m and saved code for doing so

--- R/makexy.R
+++ R/makexy.R
@@ -1,6 +1,10 @@
 #' Create input to sampler from data frame
 #' 
 #' @param res Data frame of results with columns team1, team2, score1, score2
+#' 
+#' @return list containing design matrix of teams (as numbers), matrix of scores, names of teams
+#' 
+#' @examples makexy(m)
 
 makexy=function(res)
 {
@@ -10,6 +14,14 @@ makexy=function(res)
   y=cbind(res[,3],res[,4]) 
   list(X=x,Y=y,names=levels(f))
 }
+
+#' Split vector into two halves
+#' 
+#' @param x vector to split in half
+#' 
+#' @return list of length 2, each component containing half of original vector
+#' 
+#' @examples split_half(letters[1:6])
 
 split_half=function(x)
 {
---
+++ data-raw/make_m.R
@@ -0,0 +1,2 @@
+m=read.table("mm.txt",header=T,stringsAsFactors=F)
+devtools::use_data(m,overwrite=T)
